dca9e214c4e0a2b37d627869f93b88f12353455f
aomi905 <hanna.s905@gmail.com>
2021-04-09 21:17:14 +0300

    User class

--- src/bsu/rfact/java/laba7/User.java
+++ src/bsu/rfact/java/laba7/User.java
@@ -1,7 +1,9 @@
 package bsu.rfact.java.laba7;
 
-public class Main {
+public class User {
 
+    private String name;
+    private String addres;
     public static void main(String[] args) {
 
     }
